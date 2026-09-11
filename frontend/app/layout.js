a27250a61cbb71624d90664a58a3588036cb70bc
mayconfosantos-debug <maycon.fosantos@gmail.com>
2026-09-11 19:30:33 -0300

    Add files via upload

---
+++ frontend/app/layout.js
@@ -0,0 +1,10 @@
+import './globals.css'
+
+export const metadata = {
+  title: 'Industrial Performance | H2M Consulting',
+  description: 'Industrial Performance Intelligence'
+}
+
+export default function RootLayout({ children }) {
+  return <html lang="pt-BR"><body>{children}</body></html>
+}
