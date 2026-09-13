f36013ad26145f815869ea44f5b7cf19ec0bd3a0
mayconfosantos-debug <maycon.fosantos@gmail.com>
2026-09-13 02:01:31 -0300

    Delete frontend/app/[screen]/page.js

--- frontend/app/[screen]/page.js
+++
@@ -1,5 +0,0 @@
-import DashboardClient from '../../components/DashboardClient'
-export default async function ScreenPage({ params }) {
-  const { screen } = await params
-  return <DashboardClient screen={screen}/>
-}
