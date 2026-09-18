7386c6be3c73d7f96230fd47987665b1abdb5a4c
mayconfosantos-debug <maycon.fosantos@gmail.com>
2026-09-18 11:46:33 -0300

    Add files via upload

--- frontend/app/[screen]/page.js
+++ frontend/app/[screen]/page.js
@@ -1,15 +1,5 @@
-import { Suspense } from 'react'
 import DashboardClient from '../../components/DashboardClient'
-
-function ScreenFallback() {
-  return <div style={{padding:'24px',color:'#dce9f0',background:'#061420',minHeight:'100vh'}}>Carregando Industrial Performance…</div>
-}
-
 export default async function ScreenPage({ params }) {
   const { screen } = await params
-  return (
-    <Suspense fallback={<ScreenFallback />}>
-      <DashboardClient screen={screen} />
-    </Suspense>
-  )
+  return <DashboardClient screen={screen}/>
 }
